ecba2e650522decf6dcfecd0f5f9c5fc8d1b183e
Paul Jaworski <pjaworski27@gmail.com>
2013-10-04 10:52:33 -0700

    really improve logging

--- cli.js
+++ cli.js
@@ -4,7 +4,9 @@ exports.start = function () {
     var argv = require('optimist')
         .default('environment', 'development')
         .alias('e', 'environment')
+        .usage('Start up the www-node server. \nUsage: $0')
         .argv;
+    var util = require('util');
 
     var exec = require('child_process').exec;
 
@@ -25,10 +27,17 @@ exports.start = function () {
     if (env && env.length && action && action.length) {
         var command, child;
 
-        if (action === 'start') {
+        if (action[0] === 'start') {
             command = 'NODE_ENV=' + env + ' /var/www/www-node/node_modules/.bin/forever ' + action +
-                          ' -l ' + '/var/www/www-node/logs/forever-' + currTime.toISOString() +
+                          ' -l ' + '/var/www/www-node/logs/applog/forever-' + currTime.toISOString() + '.log' +
+                          ' -o ' + '/var/www/www-node/logs/syslog/forever-' + currTime.toISOString() + '.log' +
+                          ' -e ' + '/var/www/www-node/logs/errlog/forever-' + currTime.toISOString() + '.log' +
                           ' /var/www/www-node/app.js';
+        } else if (action[0] === 'stop') {
+            command = '/var/www/www-node/node_modules/.bin/forever ' + action + ' /var/www/www-node/app.js';
+        } else {
+            console.log('Did not recognize action: ' + action[0]);
+            return;
         }
 
         child = exec(command,
